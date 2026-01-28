00addf8a74d6bdefd974f717353b48266e62848d
Mansi Singh <83419123+mansi2001-coder@users.noreply.github.com>
2026-01-28 17:49:14 +0530

    Update test_button.py

--- test_button.py
+++ test_button.py
@@ -2,21 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-# Step 1: Launch browser
+#Launch browser
 driver = webdriver.Chrome()
 
-# Step 2: Open website
+#Open website
 driver.get("https://in.mathworks.com/")
 
-# Step 3: Wait for page to load
+#Wait for page to load
 time.sleep(3)
 
-# Step 4: Find button and click
+#Find button and click
 button = driver.find_element(By.LINK_TEXT, "Explore MATLAB")
 button.click()
 
-# Step 5: Wait to see result
+#Wait to see result
 time.sleep(5)
 
-# Step 6: Close browser
+#Close browser
 driver.quit()
+
